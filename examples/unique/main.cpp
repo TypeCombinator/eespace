5482f3904ea963d54b532c96b4fc98a6c60d4c98
TypeCombinator <869119842@qq.com>
2024-12-01 00:04:47 +0800

    Add non-recursive solutions to the unique

--- examples/unique/main.cpp
+++ examples/unique/main.cpp
@@ -41,6 +41,7 @@ struct all_unique_empty {
 static_assert(sizeof(all_unique_empty) == 1);
 
 //.................................................................................................
+#if 0
 template <typename T>
 struct empty_wrap { };
 
@@ -60,7 +61,33 @@ constexpr bool is_type_pack_set_v = (sizeof(empty_box<Ts...>) == 1);
 
 template <class... Ts>
 constexpr bool is_type_pack_multiset_v = !is_type_pack_set_v<Ts...>;
+#else
+/*
+You can use lambda and parameter pack expansion to avoid recursion, please refer to the PR:
+https://github.com/tip-of-the-week/cpp/pull/160
+*/
+template <typename T>
+struct empty_wrap { };
 
+template <typename T, typename = decltype([]() -> void {})>
+struct unique_empty_box {
+    [[no_unique_address]] empty_wrap<T> e;
+};
+
+template <typename... Ts>
+struct empty_type_pack : public unique_empty_box<Ts>... { };
+
+template <typename... Ts>
+constexpr bool is_type_pack_set_v = sizeof(empty_type_pack<Ts...>) == 1;
+
+template <typename... Ts>
+constexpr bool is_type_pack_multiset_v = !is_type_pack_set_v<Ts...>;
+/*
+You can also use std::index_sequence and parameter pack expansion to implement it. 
+The following code is from a comment of the user named 'loop', please refer to the URL:
+https://godbolt.org/z/nvGrxEznb
+*/
+#endif
 
 static_assert(is_type_pack_set_v<>);
 static_assert(is_type_pack_set_v<char>);
